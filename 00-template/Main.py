ec650b2bea11e6e5ef059f7df04648c7507a0c7d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-02-20 10:38:31 +0700

    Template

---
+++ 00-template/Main.py
@@ -0,0 +1,12 @@
+# print("Hallo World")
+
+# print("Apa kabar")
+
+# print("My Name is Bayu Bagus Bagaswara")
+
+for i in range(3, 8, 1):
+    if i == 9:
+        print(i+2)
+        break
+    else:
+        print(i-2)
