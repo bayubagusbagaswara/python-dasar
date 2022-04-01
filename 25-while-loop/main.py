9632b32f563ae7b063f3cc8e4d417d793ed61453
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-01 20:07:54 +0700

    While Loop

---
+++ 25-while-loop/main.py
@@ -0,0 +1,16 @@
+# while loop
+
+# while kondisi:
+#     aksi ini
+#     aksi itu
+
+# akhir dari program
+
+angka = 0
+
+while angka < 5:
+    angka += 1  # ini untuk mengubah kondisi
+    print(f"angka sekarang -> {angka}")
+
+# ketika kondisi false, maka akan keluar dari perulangan
+print("cukuuupp")
