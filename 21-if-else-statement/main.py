470e4253df286bb4896a6350a672d31c2f48826d
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-04-01 20:06:35 +0700

    If Else Statement

---
+++ 21-if-else-statement/main.py
@@ -0,0 +1,31 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="ucup" : print("Kamu Ganteng")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+if nama == "ucup":
+    print("kamu ganteng")
+    print("kamu juga keren")
+
+print(f"terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama == "otong":
+    print("hai otong, si keren")
+else:
+    print("Ah kamu buka otong, kamu gak keren! :(")
+
+print("akhir dari program")
